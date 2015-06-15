58328612c5747f89feea4c719698d24b3c0e3453
mel678 <mel.gap981@gmail.com>
2015-06-15 13:30:27 -0500

    ultima
    nya

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('#close').tap(function(){
+			
+			<input type="button" value="Cerrar App" onclick="window.close()">;
+			navigator.notification.beep(1);
+		});
+	},false);
+});
